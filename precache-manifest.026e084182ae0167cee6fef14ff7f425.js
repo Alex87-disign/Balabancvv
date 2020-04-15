1e0af798d34beea73f46fd5f069949f79d8220cd
Yatsiuk Oleksandr <saj87@bk.ru>
2020-04-15 09:35:05 +0300

    Updates

---
+++ precache-manifest.026e084182ae0167cee6fef14ff7f425.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7393d2e57fbbb859b88d810dcad169d0",
+    "url": "/Alex87-disign/Balabancvv.git/index.html"
+  },
+  {
+    "revision": "6c867543a76febed22d3",
+    "url": "/Alex87-disign/Balabancvv.git/static/css/main.7e7d6d7f.chunk.css"
+  },
+  {
+    "revision": "2409580eaa746523413d",
+    "url": "/Alex87-disign/Balabancvv.git/static/js/2.875b2dce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Alex87-disign/Balabancvv.git/static/js/2.875b2dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c867543a76febed22d3",
+    "url": "/Alex87-disign/Balabancvv.git/static/js/main.cbe19c71.chunk.js"
+  },
+  {
+    "revision": "b6a63e0f7a4ad8842d66",
+    "url": "/Alex87-disign/Balabancvv.git/static/js/runtime-main.2311f443.js"
+  },
+  {
+    "revision": "e61a60d1e56fd5a69cce47d681b18122",
+    "url": "/Alex87-disign/Balabancvv.git/static/media/instagram.e61a60d1.png"
+  },
+  {
+    "revision": "e154da526210b69c549e5f0be902e1e2",
+    "url": "/Alex87-disign/Balabancvv.git/static/media/linkedin.e154da52.png"
+  },
+  {
+    "revision": "60b77c6d27786d2193040f290801d493",
+    "url": "/Alex87-disign/Balabancvv.git/static/media/photo.60b77c6d.jpeg"
+  },
+  {
+    "revision": "a84783664dcdd4b3b723ccb0eca8b668",
+    "url": "/Alex87-disign/Balabancvv.git/static/media/telegram.a8478366.png"
+  },
+  {
+    "revision": "da3b52a265f79c2e698644adf6bf7490",
+    "url": "/Alex87-disign/Balabancvv.git/static/media/uk.da3b52a2.png"
+  }
+]);
